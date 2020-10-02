9da5c432aae63c01a3b80fa34e7ec14e5e0d55be
Tac Tacelosky <tacman@gmail.com>
2020-10-02 06:37:18 -0400

    update to Unity 2019.4, Assets v2 and GoKit (with editor fix)

--- Assets/Plugins/GoKit/GoTweenFlow.cs
+++ Assets/Plugins/GoKit/GoTweenFlow.cs
@@ -7,15 +7,15 @@ using System.Collections;
 /// like control by inserting Tweens and setting them to start at a specific time. Note that TweenFlows do not
 /// honor the delays set within regular Tweens. Use the append/prependDelay method to add any required delays
 /// </summary>
-public class GoTweenFlow : AbstractGoTweenCollection
+public sealed class GoTweenFlow : AbstractGoTweenCollection
 {
-	public GoTweenFlow() : this( new GoTweenCollectionConfig() ) {}
+	public GoTweenFlow() : this( new GoTweenCollectionConfig() ) { }
 
-	public GoTweenFlow( GoTweenCollectionConfig config ) : base( config ) {}
+	public GoTweenFlow( GoTweenCollectionConfig config ) : base( config ) { }
 
 
 	#region internal Flow management
-	
+
 	/// <summary>
 	/// the item being added already has a start time so no extra parameter is needed
 	/// </summary>
@@ -24,27 +24,27 @@ public class GoTweenFlow : AbstractGoTweenCollection
 		// early out for invalid items
 		if( item.tween != null && !item.tween.isValid() )
 			return;
-		
+
 		if( float.IsInfinity( item.duration ) )
 		{
 			Debug.LogError( "adding a Tween with infinite iterations to a TweenFlow is not permitted" );
 			return;
 		}
-		
+
 		if( item.tween != null )
-        {
-            if (item.tween.isReversed != isReversed)
-            {
-                Debug.LogError( "adding a Tween that doesn't match the isReversed property of the TweenFlow is not permitted." );
-                return;
-            }
+		{
+			if( item.tween.isReversed != isReversed )
+			{
+				Debug.LogError( "adding a Tween that doesn't match the isReversed property of the TweenFlow is not permitted." );
+				return;
+			}
 
-            // ensure the tween isnt already live
-            Go.removeTween(item.tween);
+			// ensure the tween isnt already live
+			Go.removeTween( item.tween );
 
-            // ensure that the item is marked to play.
-            item.tween.play();
-        }
+			// ensure that the item is marked to play.
+			item.tween.play();
+		}
 
 		// add the item then sort based on startTimes
 		_tweenFlows.Add( item );
@@ -52,20 +52,20 @@ public class GoTweenFlow : AbstractGoTweenCollection
 		{
 			return x.startTime.CompareTo( y.startTime );
 		} );
-		
+
 		duration = Mathf.Max( item.startTime + item.duration, duration );
 
-        if (iterations < 0)
-            totalDuration = float.PositiveInfinity;
-        else
-            totalDuration = duration * iterations;
+		if( iterations < 0 )
+			totalDuration = float.PositiveInfinity;
+		else
+			totalDuration = duration * iterations;
 	}
-	
+
 	#endregion
-	
-	
+
+
 	#region Flow management
-	
+
 	/// <summary>
 	/// inserts a Tween and sets it to start at the given startTime
 	/// </summary>
@@ -73,11 +73,11 @@ public class GoTweenFlow : AbstractGoTweenCollection
 	{
 		var item = new TweenFlowItem( startTime, tween );
 		insert( item );
-		
+
 		return this;
 	}
-	
+
 	#endregion
-	
+
 
 }
